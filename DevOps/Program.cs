f2ddf8a0097d431bed097382ff4abe624ffbb745
Junhao Chen <junhao@hotmail.com>
2024-02-28 10:44:07 +0100

    Add DevOps project

---
+++ DevOps/Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace DevOps {
+    [ExcludeFromCodeCoverage]
+    public static class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hi");
+        }
+    }
+}
