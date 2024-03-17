0dac6ed0686570127022ab5a218d6b69016a35da
Junhao Chen <junhao@hotmail.com>
2024-03-17 15:30:04 +0100

    Add different observers

---
+++ DevOps/Observers/EmailNotifier.cs
@@ -0,0 +1,6 @@
+﻿using DevOps.Observers;
+public class EmailNotifier : INotificationObserver {
+    public void Update(string message) {
+        Console.WriteLine("Email Notification: " + message);
+    }
+}
