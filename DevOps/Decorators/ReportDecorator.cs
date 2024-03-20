0e0ae212640fbc4d345cb2d2d60f9137af968a11
Junhao Chen <junhao@hotmail.com>
2024-03-20 13:59:10 +0100

    Add report decorators

---
+++ DevOps/Decorators/ReportDecorator.cs
@@ -0,0 +1,13 @@
+﻿namespace DevOps.Decorators {
+    public abstract class ReportDecorator : IReport {
+        protected IReport report;
+
+        public ReportDecorator(IReport report) {
+            this.report = report;
+        }
+
+        public virtual void GenerateReport() {
+            report.GenerateReport();
+        }
+    }
+}
