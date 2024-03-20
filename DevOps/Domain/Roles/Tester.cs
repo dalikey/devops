0cecb667377e8d01c772df836950a96627309458
ErenAygun6 <e.aygun2@student.avans.nl>
2024-03-20 18:47:27 +0100

    strategy pattern on person class & roles

--- DevOps/Domain/Roles/Tester.cs
+++ DevOps/Domain/Roles/Tester.cs
@@ -1,4 +1,9 @@
-﻿namespace DevOps.Domain.Roles {
-    public class Tester {
+﻿using DevOps.Strategies;
+
+namespace DevOps.Domain.Roles {
+    public class Tester : IRoleStrategy {
+        public void PerformRole() {
+            Console.WriteLine("Perform tester action");
+        }
     }
 }
