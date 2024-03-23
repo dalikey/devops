c6f981ddeba9fa89934ca8fe12697701bd7d8a76
ErenAygun6 <e.aygun2@student.avans.nl>
2024-03-23 23:05:57 +0100

    Removed ISprintFactory & SprintFactory

--- DevOps/Factories/SprintFactory.cs
+++
@@ -1,8 +0,0 @@
-﻿using DevOps.Domain;
-
-namespace DevOps.Factories {
-        public interface SprintFactory {
-            Sprint CreateSprint(int id, string name, DateTime startDate, DateTime endDate);
-        }
-
-    }
