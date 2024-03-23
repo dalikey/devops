42eb0e4d9b31f56016b4720085c9cc784f51366e
Junhao Chen <junhao@hotmail.com>
2024-03-23 22:05:23 +0100

    Add creation, execution, testing state

--- DevOps/States/SprintState/SprintReleasedState.cs
+++ DevOps/States/SprintState/SprintReleasedState.cs
@@ -2,6 +2,18 @@
 
 namespace DevOps.States.SprintState {
     public class SprintReleasedState : ISprintState {
+        public void NextPhase() {
+            Console.WriteLine("Cannot proceed to next phase. Sprint already released.");
+        }
+
+        public void StartSprint() {
+            Console.WriteLine("Cannot start sprint. Sprint already released.");
+        }
+
+        public void FinishSprint() {
+            Console.WriteLine("Cannot finish sprint. Sprint already released.");
+        }
+
         public void Review(Sprint sprint) {
             Console.WriteLine("Cannot review. Sprint already released.");
         }
